a8b5c85ce77226bb8f9ec42b76a80f974ca114ec
ani-mukherjee <ani.mukherjee93@gmail.com>
2023-02-24 21:16:20 +0530

    complete

--- script.js
+++ script.js
@@ -2,25 +2,21 @@ function age() {
     var d1 = (document.getElementById('date').value);
     var m1 = (document.getElementById('mnth').value);
     var y1 = (document.getElementById('yr').value);
-    var date = new Date();
-    var d2 = date.getDate();
-    var m2 = date.getMonth();
-    var y2 = date.getFullYear();
-    var d = (d2 - d1);
-    var m = (m2 - m1)+13;
-    var y = (y2 - y1)-1;
-    if(m>11){
-      y=y+1;
-      m=0;
-  if(m1==1){
-    m=m+1;
+      var date = new Date();
+      var d2 = date.getDate();
+      var m2 = 1 + date.getMonth();
+      var y2 = date.getFullYear();
+      var daysofmonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+      if(d1 > d2){
+        d2 = d2 + daysofmonth[m2 - 1];
+        m2 = m2 - 1;
+      }
+      if(m1 > m2){
+        m2 = m2 + 12;
+        y2 = y2 - 1;
+      }
+      var d = d2 - d1;
+      var m = m2 - m1;
+      var y = y2 - y1;
+      document.getElementById('age').innerHTML = 'Your Age is '+y+' Years '+m+' Months '+d+' Days';
     }
-    if(d1>d2){
-      y=y-1;
-      m=(m2-m1)+12;
-      d=d1-d2;
-    }
-  }
-     document.getElementById('age').innerHTML = 'Your Age is '+y+' Years '+m+' Months '+d+' Days ';
- 
-  }
