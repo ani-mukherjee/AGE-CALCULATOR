1ff0b10ebbf0c36a4a2c2f767363ca1fcf74eb8a
ani-mukherjee <ani.mukherjee93@gmail.com>
2023-05-01 22:18:18 +0530

    ok

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function age() {
-    var d1 = (document.getElementById('date').value);
-    var m1 = (document.getElementById('mnth').value);
-    var y1 = (document.getElementById('yr').value);
+    var d1 = document.getElementById('date').value;
+    var m1 = document.getElementById('mnth').value;
+    var y1 = document.getElementById('yr').value;
       var date = new Date();
       var d2 = date.getDate();
       var m2 = 1 + date.getMonth();
